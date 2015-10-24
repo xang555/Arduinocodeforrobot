b0b3cd43d9fa94f1f0aa06ab591cbca1965cbfe2
xang oracle <xang.java@gmail.com>
2015-10-25 01:17:14 +0700

    Update socketServer.js

--- socketServer.js
+++ socketServer.js
@@ -1,19 +1,43 @@
 var serv = require("socket.io"); 
+var fs = require('fs');
 var io = serv.listen(3000);  
 console.log("server start"); 
 
 io.sockets.on('connection', function(socket) {  
   
-      console.log("client connect");
+    console.log("client connect");
+         
     socket.on('robot', function(data) { 
         
        //get values from phone //    
       var pin = data.pin; //pin
       var stat=data.stt; //command
       var msg= pin+" : "+stat;
+        //save file
+        var obj=null;
+fs.readFile('./store.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  obj = JSON.parse(data);
+  for(var i=0;i<obj.length;i++){
       
+      if(obj[i].pin==pin){
+          obj[i].stt=stat;
+           fs.writeFile('./store.json', JSON.stringify(obj),function (err){
+        if (err) throw err;
+        
+            });
+      }
+            
+  }
+  
+});
+   
+  
+ 
+      //============================================//  
+        
             console.log(msg);     
-            io.sockets.emit('robot', {'pin':pin,'stt':stat}); // send pin and command
+        io.sockets.emit('robot', {'pin':pin,'stt':stat}); // send pin and command
             
        //get values from pi//
        var ack=data.ack; //get values ack; ack:1 when successfully
@@ -22,7 +46,31 @@ io.sockets.on('connection', function(socket) {
         }
         
     }); //on send via robot
-  
+    //===============================================//
+    
+    socket.on('restore',function (data){
+        
+        var pin=data.pin; //pin for restore
+            var obj;
+fs.readFile('./store.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  obj = JSON.parse(data);
+  for(var i=0;i<obj.length;i++){
+      if(obj[i].pin==pin){
+          socket.emit('restore',obj[i]);
+                    break;
+      }
+      
+      
+  }//end for
+ 
+});
+    
+        
+                
+    });
+    
+    
     
     socket.on('disconnect', function() {
       //io.sockets.emit('disconnect',{"name":"disconnect"});
@@ -31,3 +79,10 @@ io.sockets.on('connection', function(socket) {
     });//end disconnect
     
 }); //on connect
+
+function updatedb(val,status){
+ var i=0;   
+
+        
+}//end function
+
