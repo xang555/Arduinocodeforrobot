3549e76f2ef7301b67e1e2fef9c93e665e326431
xang oracle <xang.java@gmail.com>
2015-11-21 13:38:47 +0700

    Update socketServer.js

--- socketServer.js
+++ socketServer.js
@@ -7,6 +7,20 @@ io.sockets.on('connection', function(socket) {
   
     console.log("client connect");
          
+       //Send values To Pi //   
+	socket.on('pi', function(d) {
+	console.log("client PI connect");
+	var obj;
+	fs.readFile('./store.json', 'utf8', function (err, data) {
+	if (err) throw err;
+   	obj = JSON.parse(data); 
+  	socket.emit('pi',obj);
+ 
+	});
+	});
+	//end on Send values To Pi     
+         
+                  
     socket.on('robot', function(data) { 
         
        //get values from phone //    
@@ -17,8 +31,6 @@ io.sockets.on('connection', function(socket) {
         //=====save file======//
         readwritejson(pin,stat,whosend);
       
-      //=================send passing sendmessage===========================//  
-        
             console.log(msg);     
         io.sockets.emit('robot', {'pin':pin,'stt':stat,'name':whosend}); // send pin and command
             
@@ -64,6 +76,92 @@ io.sockets.on('connection', function(socket) {
      console.log(username +" clien disconnect");
      
     });//end disconnect
+    
+    //==============restore toilet ligth==============//
+    
+    socket.on('toilet',function (data){
+        var jsonforsend="[";
+        var mjson=data; //get json pin toilet [{'pin':4},{'pin':5}]
+        var storejson=getstorejson(); //get store json
+        for(var i=0;i<mjson.length;i++){
+            for(var j=0;j<storejson.length;j++){
+                
+                if(mjson[i].pin==storejson[j].pin){
+                   jsonforsend+="{\"pin\":"+storejson[j].pin+",\"stt\":\""+storejson[j].stt+"\",\"name\":\""+storejson[j].name+"\"}";
+                                     
+                }//end if
+                                
+            }//end for in
+             if(i!=3){
+                    jsonforsend+=",";
+                }//end if
+            
+        }//end for out
+        
+        jsonforsend+="]";
+        
+        var jsontoilet=JSON.parse(jsonforsend);
+        
+        socket.emit('toilet',jsontoilet); //emit pass toilet
+        
+    }); //open toilet
+    
+    socket.on('bed',function (data){
+        
+         var jsonforsend="[";
+        var mjson=data; //get json pin toilet [{'pin':4},{'pin':5}]
+        var storejson=getstorejson(); //get store json
+        for(var i=0;i<mjson.length;i++){
+            for(var j=0;j<storejson.length;j++){
+                
+                if(mjson[i].pin==storejson[j].pin){
+                  jsonforsend+="{\"pin\":"+storejson[j].pin+",\"stt\":\""+storejson[j].stt+"\",\"name\":\""+storejson[j].name+"\"}";
+                                     
+                }//end if
+                                
+            }//end for in
+             if(i!=2){
+                    jsonforsend+=",";
+                }//end if
+            
+        }//end for out
+        
+        jsonforsend+="]";
+        
+        var jsontoilet=JSON.parse(jsonforsend);
+         socket.emit('bed',jsontoilet); //emit pass bed
+               
+        
+    });// event on bed
+    
+    socket.on('way',function (data){
+        
+            var jsonforsend="[";
+        var mjson=data; //get json pin way [{'pin':4},{'pin':5}]
+        var storejson=getstorejson(); //get store json
+        for(var i=0;i<mjson.length;i++){
+            for(var j=0;j<storejson.length;j++){
+                
+                if(mjson[i].pin==storejson[j].pin){
+                  jsonforsend+="{\"pin\":"+storejson[j].pin+",\"stt\":\""+storejson[j].stt+"\",\"name\":\""+storejson[j].name+"\"}";
+                                     
+                }//end if
+                                
+            }//end for in
+             if(i!=1){
+                    jsonforsend+=",";
+                }//end if
+            
+        }//end for out
+        
+        jsonforsend+="]";
+        
+        var jsontoilet=JSON.parse(jsonforsend);
+        
+        socket.emit('way',jsontoilet); //emit pass way
+        
+        
+    });//event on way
     
 }); //on connect
 
@@ -164,4 +262,10 @@ function savewhendiscon(username){
     
 }//end savewhendiscon
 
+function getstorejson(){
+    var dbstore = fs.readFileSync('./store.json');
+  var dbjson = JSON.parse(dbstore);
+    return dbjson;   
+    
+}//get store json
 
